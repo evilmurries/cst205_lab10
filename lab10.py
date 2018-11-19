03aab22a51a2902c248e254b8906919ecd26317d
evilmurries <cpiwarski00@gmail.com>
2018-11-19 10:19:14 -0800

    updated comments

--- lab10.py
+++ lab10.py
@@ -21,16 +21,18 @@ def hangman():
   
   #initialize variables
   word = 'sausage'
-  wordLength = len(word)
+  wordLength = len(word) #third string op. Getting the length of a string.
   guessCount = 6
   guessedLetters = ''
   incorrectGuesses = 0
+  
+  #used multiple strings because string blocks were having issues with JES
   description = 'To play hangman you must guess characters to fill in the word. '
   rules1 = 'You are allowed six incorrect guesses. '
   rules2 = 'When you guess a correct letter then it will fill in one of the blank spaces.\n'
   
   #print directions and status
-  print description + rules1 + rules2
+  print description + rules1 + rules2 #string op. Concatenation
   print ('Total Amount of Guesses: %d' % guessCount) #String Formatting. First String Op
   
   #begin game
@@ -41,7 +43,7 @@ def hangman():
     for character in word:
       if character in guessedLetters:
         sys.stdout.write(character)#This allows for the to print Without Newline or Space 
-        sys.stdout.flush()
+        sys.stdout.flush() #Source for this is located at the import statement
         characterCount += 1
       else:
         sys.stdout.write('_ ')
@@ -52,15 +54,15 @@ def hangman():
     print ('Incorrect Guesses: %d' % incorrectGuesses) 
   
     badCharacter = True
-    while badCharacter: #determining if they guessed the right character or not 
+    while badCharacter: #repeats guess process until they submit a valid character
       guess = requestString('Enter in a letter: ')
       print 'Your guess is %s' % guess
-      if checkInput(guess): # if this passes it determines if its good
+      if checkInput(guess): #checks if input is a single character
         badCharacter = False
-        if guess in word: #string find method. Third String op.
-          guessedLetters += guess #string concatenation. Second String Op
+        if guess in word: #string operation. Using in to search a string
+          guessedLetters += guess 
           badCharacter = False
-        elif guessedLetters.count(guess) > 0: #string count method. Fourth string op.
+        elif guessedLetters.count(guess) > 0: #string count method. Another string op
           badCharacter = True
           print 'Letter already guessed. Try again.'
         else:
@@ -71,7 +73,7 @@ def hangman():
         badCharacter = True
         print 'Incorrect input. Enter in a single letter'
       print 'You have used %d out of %d guesses' % (incorrectGuesses, guessCount)
-  if incorrectGuesses == 6:
+  if incorrectGuesses == 6: # determines whether the player won or lost
   	print 'Sorry please play again'
   else:
   	print 'You win!'
